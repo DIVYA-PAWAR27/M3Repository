02d7e6540634a1a5ca7e21f640125f7cc3da6069
Admin <Admin@DESKTOP-EAILKN8>
2025-03-21 12:33:22 +0530

    First Version 2.0

---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchBMW_006_Test.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchBMW_006_Test {
+  @Test
+  public void LaunchBMW() {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://www.bmw.in/en/index.html");
+  
+
+  }
+}
---
+++ com.onlineShoppingPortal/src/test/java/testPackage/TC_LaunchKTM_003_Test.java
@@ -0,0 +1,21 @@
+package testPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_LaunchKTM_003_Test {
+  @Test
+  public void LaunchKTM() {
+	  WebDriver driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	  driver.get("https://www.ktm.com/en-in.html");
+  
+
+
+  }
+  }
+
